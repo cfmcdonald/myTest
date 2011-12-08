713fd3e7ec6bedfea769fb9daa085821dfb7ac16
unknown <cmcdonald@FS4652.ecfs.internal>
2011-12-08 10:20:00 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Message::Application.config.secret_token = 'c521f3d01825387309316fed8c2f09f73689b2998d8e500d12ed4f2592c2f32e46c60bdf575fef6209204ceea61d27563ab067b6f3bd8f3bab6537c125c19b60'
